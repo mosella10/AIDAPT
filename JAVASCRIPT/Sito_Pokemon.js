c53b29c03916a67e2b337d90c8baa6344200c0a9
mosella10 <alessandromosella47@gmail.com>
2024-03-07 10:51:38 +0100

    pokeCommit

--- JAVASCRIPT/Sito_Pokemon.js
+++ JAVASCRIPT/Sito_Pokemon.js
@@ -1,3 +1,18 @@
-function getPokemonInfo() {
-    let pokemonId  = document.getElementById(pokemonId)
-}
+// Funzione per ottenere i dati del Pokémon dall'API
+function getPokemonData() {
+    var pokemonId = document.getElementById("pokemonId").value;
+    var apiUrl = "https://pokeapi.co/api/v2/pokemon/" + pokemonId;
+  
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        // Visualizza i dati del Pokémon
+        document.getElementById("pokemonName").innerText = "Nome: " + data.name;
+        document.getElementById("pokemonImage").src = data.sprites.front_default;
+        document.getElementById("pokemonTypes").innerText = "Tipi: " + data.types.map(type => type.type.name).join(", ");
+        document.getElementById("pokemonAbilities").innerText = "Abilità: " + data.abilities.map(ability => ability.ability.name).join(", ");
+      })
+      .catch(error => {
+        console.log("Errore:", error);
+      });
+  }
